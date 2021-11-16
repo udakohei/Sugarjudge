038f45c0f1bee12eed7b38d9eb28f29f2e0afb87
udakohei <udauda0211@gmail.com>
2021-11-17 00:19:34 +0900

    Add: 画像アップロード機能 アップロードまで

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,7 +1,25 @@
 class MealsController < ApplicationController
   def new
+    @meal = Meal.new
   end
 
   def create
+    begin
+        @meal = Meal.new(meal_params)
+      if @meal.save
+        redirect_to root_path, success: t('.success')
+      else
+        flash.now[:danger] = t('.failure')
+        render :new
+      end
+    rescue ActionController::ParameterMissing 
+      redirect_to new_meal_path, danger: t('.failure')
+    end
+  end
+
+  private
+  
+  def meal_params
+    params.require(:meal).permit(:meal_image)
   end
 end
