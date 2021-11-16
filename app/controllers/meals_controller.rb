d35e7f6d37b97aa921c3bdc1fb78cf4b999fba58
udakohei <udauda0211@gmail.com>
2021-11-16 23:30:07 +0900

    Add: carrier_wave紐付け、画像アップロードページ遷移

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,7 @@
+class MealsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, success: t('.success')
+      redirect_to new_meal_path, success: t('.success')
     else
       flash.now[:danger] = t('.failure')
       render :new
