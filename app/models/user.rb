566e0cffb78a6b53f795019f9bf60b15b4e2457e
udakohei <udauda0211@gmail.com>
2021-11-18 21:47:36 +0900

    Fix: 必要カロリー量を割り出すメソッドを書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,70 +10,20 @@ class User < ApplicationRecord
   validates :physical_activity_level, presence: true
 
   def required_calorie
-    if male? && low?
-      if age >= 18 && age <= 49
-        766
-      elsif age >= 50 && age <= 64
-        733
-      elsif age >= 65 && age <= 74
-        683
-      elsif age >= 75
-        600
-      end
-    elsif male? && normal?
-      if age >= 18 && age <= 29
-        883
-      elsif age >= 30 && age <= 49
-        900
-      elsif age >= 50 && age <= 64
-        886
-      elsif age >= 65 && age <= 74
-        800
-      elsif age >= 75
-        700
-      end
-    elsif male? && high?
-      if age >= 18 && age <= 49
-        1016
-      elsif age >= 50 && age <= 64
-        983
-      elsif age >= 65
-        916
-      end
-    elsif female? && low?
-      if age >= 18 && age <= 29
-        566
-      elsif age >= 30 && age <= 49
-        583
-      elsif age >= 50 && age <= 64
-        550
-      elsif age >= 65 && age <= 74
-        516
-      elsif age >= 75
-        466
-      end
-    elsif female? && normal?
-      if age >= 18 && age <= 29
-        666
-      elsif age >= 30 && age <= 49
-        683
-      elsif age >= 50 && age <= 64
-        650
-      elsif age >= 65 && age <= 74
-        616
-      elsif age >= 75
-        550
-      end
-    elsif female? && high?
-      if age >= 18 && age <= 29
-        766
-      elsif age >= 30 && age <= 49
-        783
-      elsif age >= 50 && age <= 64
-        750
-      elsif age >= 65
-        700
-      end
+    if male?
+      (7.16 - 0.0138 * age - 0.4235) * 238 * physical_activity_level_value / 3
+    else
+      (6.19 - 0.0138 * age - 0.9708) * 238 * physical_activity_level_value / 3
+    end
+  end
+
+  def physical_activity_level_value
+    if low?
+      1.5
+    elsif normal?
+      1.75
+    else
+      2
     end
   end
 end
