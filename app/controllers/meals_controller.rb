2ef73badc6cf940b65293f6d84fa23f49c53bb02
udakohei <udauda0211@gmail.com>
2021-11-20 05:03:38 +0900

    Add: GoogleAPIで画像解析機能追加

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -22,6 +22,8 @@ class MealsController < ApplicationController
 
   def edit
     @meal = current_user.meals.find(params[:id])
+    sent_image = File.open(@meal.meal_image.file.file)
+    @analyzed_foods = image_analysis(sent_image)
     @foods = Food.all
   end
 
@@ -44,5 +46,29 @@ class MealsController < ApplicationController
   
   def meal_params
     params.require(:meal).permit(:meal_image)
+  end
+
+  def image_analysis(meal_image)
+    # image_path = File.expand_path('../', __FILE__)+"/steak.jpeg"
+
+    image_annotator = Google::Cloud::Vision.image_annotator
+
+    translate = Google::Cloud::Translate::V2.new
+
+    response = image_annotator.label_detection(
+      image:   meal_image,
+    )
+
+    results = []
+
+    response.responses.each do |res|
+      res.label_annotations.each do |label|
+        translation = translate.translate label.description.downcase, to: 'ja'
+        results << translation.text
+        # puts label.description
+      end
+    end
+    
+    p results
   end
 end
