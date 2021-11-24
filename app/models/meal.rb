71a30e220bcffe1ac2532a5d7b7b57babe8d32d0
udakohei <udauda0211@gmail.com>
2021-11-25 03:50:16 +0900

    Add: 判定結果詳細ページ　文字ベースでの内容実装

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -18,4 +18,48 @@ class Meal < ApplicationRecord
   def balance_of_payments
     calorie_intake - user.required_calorie.round
   end
+
+  def red?
+    balance_of_payments > 0
+  end
+
+  def result
+    if red?
+      "カロリー赤字です"
+    else
+      "カロリー黒字です"
+    end
+  end
+
+  def result_color
+    if red?
+      'text-danger'
+    else
+      'text-dark'
+    end
+  end
+
+  def balance_of_payments_with_sign
+    if red?
+      "+#{balance_of_payments} kcal"
+    else
+      "#{balance_of_payments} kcal"
+    end
+  end
+
+  def result_message
+    if red?
+      "赤字が増えすぎるとカロリー破産してしまいます。気をつけてください。"
+    else
+      "安定した黒字は確実にあなたの資産となります。"
+    end
+  end
+
+  def result_meal_title
+    if foods.length == 1
+      "『#{foods.first.name}』"
+    else
+      "『#{foods.order(calorie: :desc).first.name}と諸々』"
+    end
+  end
 end
