425589342d48edb21393b2d61a8a3cba56583874
udakohei <udauda0211@gmail.com>
2021-12-02 03:34:38 +0900

    Add: その他のチェック欄を表示切り替え機能実装 一つでもチェックが入ったらjQueryで表示させる

--- app/javascript/checkbox.js
+++ app/javascript/checkbox.js
@@ -1,26 +1,15 @@
 $(function(){
-  // 初期状態のボタンは無効
-  $("#btn1").prop("disabled", true);
-    // チェックボックスの状態が変わったら（クリックされたら）
-    
-        // チェックされているチェックボックスの数
-        if ($(".chk:checked").length > 0) {
-          // ボタン有効
-          $("#btn1").prop("disabled", false);
-        } else {
-          // ボタン無効
-          $("#btn1").prop("disabled", true);
-        };
+  $(".js-checkbox-others").hide();
+  
+  function changeDisplay() {
+    if ($(".chk:checked").length > 0) {
+      $(".js-checkbox-others").show();
+    } else {
+      $(".js-checkbox-others").hide();
+    };
+  };
 
+  changeDisplay();
 
-    $("input[type='checkbox']").on('change', function () {
-      // チェックされているチェックボックスの数
-      if ($(".chk:checked").length > 0) {
-        // ボタン有効
-        $("#btn1").prop("disabled", false);
-      } else {
-        // ボタン無効
-        $("#btn1").prop("disabled", true);
-      }
-  });
+  $("input[type='checkbox']").on('change', changeDisplay);
 });
