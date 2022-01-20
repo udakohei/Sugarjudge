837a78f02ab34823b299d19231ade441833b5b2d
udakohei <udauda0211@gmail.com>
2022-01-20 12:50:32 +0900

    Add: ユーザー編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
   resources :meals
 end
