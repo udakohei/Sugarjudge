f84e9296dd1e8940d612ae5cb59957bca6b80b17
udakohei <udauda0211@gmail.com>
2022-02-10 15:48:55 +0900

    Add: ユーザー詳細ページ追加 遷移するリンクと判定結果のでたその人の食事一覧が表示されるよう実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @user = User.login.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @meals = @user.meals.with_result.includes(:foods).order(created_at: :desc).page(params[:page]).per(24)
+  end
+
   def create
     @user = User.new(user_params)
     if @user.guest?
