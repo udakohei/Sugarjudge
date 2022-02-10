3f45e514abb59aa2b4c3a91b8205f433ce3fb1a5
udakohei <87481460+udakohei@users.noreply.github.com>
2022-02-10 17:05:08 +0900

    Revert "ユーザー詳細ページ追加"

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create update show]
+  resources :users, only: %i[new create update]
   resources :meals
 end
