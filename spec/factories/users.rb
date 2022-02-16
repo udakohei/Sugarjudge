4c8069b9f099e5cf920db4e8f31b158bee0fed42
udakohei <udauda0211@gmail.com>
2022-02-17 04:26:45 +0900

    Add: FactoryBotを使ってテストデータ生成するよう設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'テスト太郎' }
+    gender { :male }
+    limit_level { :low }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,53 +2,32 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with name, gender, limit_level" do
-    user = User.new(
-      name: 'テスト太郎',
-      gender: :male,
-      limit_level: :low,
-    )
+    user = build(:user)
     expect(user).to be_valid
     expect(user.role).to eq 'guest'
   end
 
   it "is invalid without name" do
-    user = User.new(
-      name: nil,
-      gender: :male,
-      limit_level: :low,
-    )
-    expect(user).to be_invalid
-    expect(user.errors[:name]).to eq ['を入力してください']
+    user_without_name = build(:user, name: '')
+    expect(user_without_name).to be_invalid
+    expect(user_without_name.errors[:name]).to eq ['を入力してください']
   end
 
   it "is invalid without gender" do
-    user = User.new(
-      name: 'テスト太郎',
-      gender: nil,
-      limit_level: :low,
-    )
-    expect(user).to be_invalid
-    expect(user.errors[:gender]).to eq ['を入力してください']
+    user_without_gender = build(:user, gender: nil)
+    expect(user_without_gender).to be_invalid
+    expect(user_without_gender.errors[:gender]).to eq ['を入力してください']
   end
 
   it "is invalid without limit_level" do
-    user = User.new(
-      name: 'テスト太郎',
-      gender: :male,
-      limit_level: nil,
-    )
-    expect(user).to be_invalid
-    expect(user.errors[:limit_level]).to eq ['を入力してください']
+    user_without_limit_level = build(:user, limit_level: nil)
+    expect(user_without_limit_level).to be_invalid
+    expect(user_without_limit_level.errors[:limit_level]).to eq ['を入力してください']
   end
 
   it "is invalid without role" do
-    user = User.new(
-      name: 'テスト太郎',
-      gender: :male,
-      limit_level: :low,
-      role: nil
-    )
-    expect(user).to be_invalid
-    expect(user.errors[:role]).to eq ['を入力してください']
+    user_without_role = build(:user, role: nil)
+    expect(user_without_role).to be_invalid
+    expect(user_without_role.errors[:role]).to eq ['を入力してください']
   end
 end
